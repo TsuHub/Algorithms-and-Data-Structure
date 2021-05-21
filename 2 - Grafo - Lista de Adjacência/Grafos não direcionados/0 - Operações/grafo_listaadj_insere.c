b3b6d77ad26809246bc820dfd1f9809fd56f19d5
TsuHub <tsuy_1988@hotmail.com>
2021-05-21 13:47:15 -0300

    Até a função de inserção de arestas, está funcionando tudo perfeitamente.

--- 2 - Grafo - Lista de Adjacência/Grafos não direcionados/0 - Operações/grafo_listaadj_insere.c
+++ 2 - Grafo - Lista de Adjacência/Grafos não direcionados/0 - Operações/grafo_listaadj_insere.c
@@ -8,20 +8,37 @@ bool insereAresta(Grafo* grafo, Peso peso, int v1, int v2)
 	if (!(verificaValidadeVertice(grafo, v1) && verificaValidadeVertice(grafo, v2))){
 		return false;
 	}
-	printf("AQUI\n");
 
-	//if(!(grafo->listaAdj = (Aresta**) calloc(1, sizeof(Aresta*)))){
-	if(!(grafo->listaAdj = (Apontador*) calloc(1, sizeof(Aresta)))){	
+	Apontador p;						// forma 2
+	
+	//if(!(grafo->listaAdj = (Aresta**) calloc(1, sizeof(Aresta*)))){	// forma 1
+	//if(!(grafo->listaAdj = (Apontador*) calloc(1, sizeof(Aresta)))){	// forma 1
+	
+	// forma 2
+	if(!(p = (Apontador) calloc(1, sizeof(Aresta)))){		
 	   	fprintf(stderr, "ERRO: Falha na alocação de memória na função insere Aresta\n");
         return false;
    	}
+   	
+   	p->vdest = v2;						// forma 2
+   	p->peso = peso;						// forma 2
+   	p->prox = grafo->listaAdj[v1];		// forma 2
+   	
+   	grafo->listaAdj[v1] = p;			// forma 2
+   	grafo->numArestas++;				// forma 2
 	
-	if (listaAdjVazia(grafo, v1)){
-		grafo->listaAdj[v1]->vdest = v2;
-		grafo->listaAdj[v1]->peso = peso;
-		grafo->listaAdj[v1]->prox = grafo->listaAdj[v2];
-	   	printf("AQUI TB\n");
-	}
+	   
+    /*
+	   Este é o uso da forma 1, onde faço uso direto da estruturas de
+       Grafo, mas esta maneira não funciona. Aparentemente tenho que
+	   fazer um ponteiro 'p' do tipo Apontador e depois passar 'p'
+	   para grafo->listaAdj[v1]. 
+	*/
+	//grafo->listaAdj[v1]->vdest = v2;						// forma 1
+	//grafo->listaAdj[v1]->peso = peso;						// forma 1
+	//grafo->listaAdj[v1]->prox = grafo->listaAdj[v1];		// forma 1
+	//grafo->numArestas++;									// forma 1
+
 	printf("A aresta de peso %d foi inserida entre os nós %d e %d.\n", peso, v1, v2);
 	
 	return true;
